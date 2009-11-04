dcbf09548a2d3ae8b75ffb65459f9c70dcab0e7a
Sam Aaron <samaaron@gmail.com>
2009-11-04 10:09:31 +0000

    add .gitignore file, update description of vlad:if:update_crontab to be app agnostic and remove empty and unsed file

--- lib/if-vlad/update_crontab.rb
+++ lib/if-vlad/update_crontab.rb
@@ -1,6 +1,6 @@
 namespace :vlad do
   namespace :if do
-    desc "Update the crontab for Ideaman's schedule (in ./configs/schedule.rb)"
+    desc "Update the crontab for the app's whenever schedule (stored in ./configs/schedule.rb)"
     remote_task :update_crontab, :roles => :web do
       run "cd #{current_path} && whenever --update-crontab #{application} --set 'environment=#{environment}&cron_log=#{shared_path}/log/cron.log'"
     end
